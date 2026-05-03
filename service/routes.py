d3b849f8337caf7ad30abc2d024f86be5a555eda
verlineguo <verlineguo34@gmail.com>
2026-05-03 06:13:11 -0400

    completed ci build

--- service/routes.py
+++ service/routes.py
@@ -89,7 +89,7 @@ def get_account(account_id):
     account = Account.find(account_id)
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
-    
+
     return account.serialize(), status.HTTP_200_OK
 
 
@@ -106,7 +106,7 @@ def update_accounts(account_id):
     account = Account.find(account_id)
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
-    
+
     account.deserialize(request.get_json())
     account.update()
     return account.serialize(), status.HTTP_200_OK
@@ -141,4 +141,3 @@ def check_content_type(media_type):
         status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
         f"Content-Type must be {media_type}",
     )
-    
